85acc36d6c45234e704ec79b100bebcd71334399
Mark Morley <mmorley0395@gmail.com>
2024-08-09 15:54:20 -0400

    black formatting

--- tests/test_gtfs.py
+++ tests/test_gtfs.py
@@ -5,18 +5,17 @@ from pygtfs import Schedule
 
 from sqlalchemy.orm import Query
 from dotenv import dotenv_values
+
 config = dotenv_values("../.env")
 
-class TestFeed(unittest.TestCase):
 
+class TestFeed(unittest.TestCase):
     def setUp(self):
         self.schedule = Schedule(config.get("DB_URI"))
 
     def test_feed_notnull(self):
         self.assertTrue(len(self.schedule.routes) > 0)
-        
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-    
