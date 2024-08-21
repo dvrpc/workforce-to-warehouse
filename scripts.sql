3a1eadc42e8809d57706ec74db05f96cd18279da
Mark Morley <mmorley0395@gmail.com>
2024-08-21 10:42:04 -0400

    add pieces to script while exploring data in q

--- scripts.sql
+++ scripts.sql
@@ -14,27 +14,37 @@ on b.service_id=c.service_id
 where b.stop_id='43422'
 and c.sunday=true;
 
+-- prep stops by adding geom based on xy
+ALTER TABLE stops ADD COLUMN geom geometry(Point, 4326);
+UPDATE stops SET geom = ST_SetSRID(ST_MakePoint(stop_lon, stop_lat), 4326;
+
+
+--install uuid extension to make unique id, necessary for looking at views in qgis
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 --select stops that are accessible from original stop
+create or replace view mystops as 
 with 
 	mystops as (
 		select 
+			uuid_generate_v4() AS st_id,
 			a.stop_id, 
 			b.trip_id, 
 			b.arrival_time, 
 			b.departure_time,
+			c.service_id,
 			c.route_id,
-			a.stop_lat, 
-			a.stop_lon 
+			a.geom
 		from stops a
 		inner join stop_times b
 		on a.stop_id=b.stop_id
 		inner join trips c
 		on b.trip_id=c.trip_id
+		where c.service_id='2' -- represents 7/14/2024 in njt gtfs
 		order by a.stop_id asc, b.arrival_time asc
 	),
 	myroutes as (
-		select route_id from trips a
+		select route_id as route_id_trips from trips a
 		inner join stop_times b
 		on a.trip_id=b.trip_id
 		where b.stop_id='43422'
@@ -42,5 +52,5 @@ with
 	)
 select * from mystops a
 inner join myroutes b
-on a.route_id=b.route_id;
+on a.route_id=b.route_id_trips;
 
