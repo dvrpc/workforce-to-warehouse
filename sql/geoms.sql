cd294e442ce5e0a94974351b8e4becfaaafa3d2a
Mark Morley <mmorley0395@gmail.com>
2024-10-08 16:11:42 -0400

    add if not exists

--- sql/geoms.sql
+++ sql/geoms.sql
@@ -2,6 +2,6 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS pgrouting;
 
 -- prep stops by adding geom based on xy
-ALTER TABLE stops ADD COLUMN geom geometry(Point, 4326);
+ALTER TABLE stops ADD COLUMN if not exists geom geometry(Point, 4326);
 UPDATE stops SET geom = ST_SetSRID(ST_MakePoint(stop_lon, stop_lat), 4326);
 
