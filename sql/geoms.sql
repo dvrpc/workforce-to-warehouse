6202b646ff021e51f76d4beaa191d37ebf90d7b2
Mark Morley <mmorley0395@gmail.com>
2024-10-10 11:29:37 -0400

    create lines from points

--- sql/geoms.sql
+++ sql/geoms.sql
@@ -1,7 +1,23 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS pgrouting;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 
 -- prep stops by adding geom based on xy
 ALTER TABLE stops ADD COLUMN if not exists geom geometry(Point, 4326);
 UPDATE stops SET geom = ST_SetSRID(ST_MakePoint(stop_lon, stop_lat), 4326);
 
+
+create table if not exists line_geoms as
+SELECT 
+    uuid_generate_v4() as st_id,
+    feed_id,
+    shape_id,
+    ST_MakeLine(
+        ST_MakePoint(shape_pt_lon, shape_pt_lat)
+        ORDER BY shape_pt_sequence
+    ) AS geom
+FROM shapes
+GROUP BY feed_id, shape_id;
+
+update line_geoms set geom = ST_SetSRID(geom, 4326);
