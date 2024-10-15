c9e77ce37e429014e2a973c4d1994c49af8f1ae0
Mark Morley <mmorley0395@gmail.com>
2024-10-15 17:57:59 -0400

    WORKING yay its now grabbing all possible trips from bbox and no unexpected

--- sql/trips.sql
+++ sql/trips.sql
@@ -27,8 +27,7 @@ from origin_stops a
 inner join stop_times b
     on a.stop_id=b.stop_id
     and a.feed_id=b.feed_id
-where a.feed_id=b.feed_id
-and a.stop_id='30408';
+where a.feed_id=b.feed_id;
 
 
 drop table if exists origin_trips;
@@ -40,48 +39,11 @@ select
     c.geom
 from test_stops a
 inner join trips b
-on a.trip_id=b.trip_id
+    on a.trip_id=b.trip_id
+    and a.feed_id=b.feed_id
 inner join line_geoms c
-on b.shape_id=c.shape_id
+    on b.shape_id=c.shape_id
+    and b.feed_id=c.feed_id
 where a.feed_id=b.feed_id;
-
-
-
-
--- drop table if exists origin_stop_times;
--- create table origin_stop_times as
--- select 
---     a.stop_id, 
---     a.feed_id, 
---     c.trip_id,
---     c.departure_time,
---     a.geom
--- from origin_stops a
--- inner join stop_times c
--- on a.stop_id=c.stop_id
--- where a.feed_id=c.feed_id;
-
--- drop table if exists mytrips;
--- create table mytrips as 
--- select 
---     a.trip_id, 
---     c.geom
--- from trips a
--- inner join origin_stops b
--- on a.trip_id=b.trip_id
--- inner join line_geoms c
--- on a.shape_id=c.shape_id
--- where b.departure_time BETWEEN '8:00:00' and '10:00:00';
-
-
-
-
-
-
-
-
-
-
-
 
 
